b99f0ed705f5159ff4f1fafc79651902b986ee1e
Son1cPower <negriystas@gmail.com>
2025-07-07 12:19:19 +0300

    added

---
+++ docker-run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+if [ $# -ne 1 ]; then
+    echo "Usage: ./docker-run.sh <SCRIPT_NAME>"
+    exit 1
+fi
+
+SCRIPT_NAME=$1
+TAG_NAME="$(basename -s .js $SCRIPT_NAME)-$(date +%s)"
+
+docker compose run --rm -T k6 run "$SCRIPT_NAME" \
+  --tag testid=$TAG_NAME \
+  --env ENVIRONMENT=bugfest \
+  --env WORKLOAD=smoke
